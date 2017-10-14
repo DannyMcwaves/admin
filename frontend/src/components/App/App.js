10358f2b2e3ff9b4634e2c1ccc6d99e9ae72c9a6
dannymcwaves <dannymcwaves96@gmail.com>
2017-10-14 18:01:53 +0000

    main app component which houses other components

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {WindowGridFluid} from "../lib/containers";
+import Homepage from './Homepage';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <WindowGridFluid className="App">
+        <Homepage/>
+      </WindowGridFluid>
+    );
+  }
+
+}
+
+export default App;
