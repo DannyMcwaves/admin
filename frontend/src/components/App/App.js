3f9892b988bbc52193e2c23585bad596bf872b99
dannymcwaves <dannymcwaves96@gmail.com>
2017-10-16 14:37:26 +0000

    separated homepage into it's own component and dir and refactored the main app

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {WindowGridFluid} from "../lib/containers";
-import Homepage from './Homepage';
+import Homepage from '../homepage/Homepage';
 import './App.scss';
 
 class App extends Component {
