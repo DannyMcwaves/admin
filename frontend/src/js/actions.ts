fc13a46d03cd342f19663df151f7bf23794b8216
dannymcwaves <dannymcwaves96@gmail.com>
2017-10-17 14:56:00 +0000

    contstants of actions to be dispatched

---
+++ frontend/src/js/actions.ts
@@ -0,0 +1,9 @@
+// this are action constants the actions that are dispatched.
+const LOADING:string = 'LOADING';
+const NOT_LOADING:string = 'NOT_LOADING';
+const UPDATE_CACHE:string = 'UPDATE_CACHE';
+const ADD_LOADED_USERS:string = 'ADD_LOADED_USERS';
+const HAS_CACHE:boolean = true; // this action is dispacthed when the user stats is properly loaded.
+const HAS_NO_CACHE:boolean = false; // this is fired at the initial stage.
+
+export {LOADING, NOT_LOADING, UPDATE_CACHE, ADD_LOADED_USERS, HAS_CACHE, HAS_NO_CACHE};
