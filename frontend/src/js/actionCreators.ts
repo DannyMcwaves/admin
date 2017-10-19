77c34dab985a25f614231f5a192cb785d1d38a5b
dannymcwaves <dannymcwaves96@gmail.com>
2017-10-19 16:44:06 +0000

    redux action creators and actions that are to be dispacthed

--- frontend/src/js/actionCreators.ts
+++ frontend/src/js/actionCreators.ts
@@ -1,9 +1,30 @@
 import {LoadAction, UserAction} from './interfaces';
-import {LOADING, NOT_LOADING, UPDATE_CACHE, ADD_LOADED_USERS} from './actions';
+import {LOADING, NOT_LOADING, UPDATE_CACHE, ADD_LOADED_USERS, USER} from './actions';
 
 const Loading:LoadAction = {type: LOADING};
 const NotLoading:LoadAction = {type: NOT_LOADING};
-const UpdateCache = (id, data):UserAction => ({type: UPDATE_CACHE, _id:id, data: data})
-const AddLoadedUsers = (id, data):UserAction => ({type: ADD_LOADED_USERS, _id: id, data: data})
+const AddLoadedUsers = (id, data):UserAction => ({type: ADD_LOADED_USERS, _id: id, data: data});
 
-export {Loading, NotLoading, UpdateCache, AddLoadedUsers};
+const getDataAndDispatch = ($, url, data, history) => (dispatch) => {
+    dispatch(Loading);
+    return $.post({ url: url, data: data, dataType: 'json' }).done((data) => {
+        dispatch({type: USER, data: data});
+        history.push('/users');
+    }).fail(err => {
+        console.log(err.responseJSON);
+    }).always(_ => {
+        dispatch(NotLoading);
+    });
+};
+
+const dispatchSignout = ($, url, history) => (dispatch) => {
+    dispatch(Loading);
+    $.get(url).done(data => {
+        dispatch({type: USER, data: {}});
+        history.push(data.path);
+    }).fail(err => {
+        console.log(err.responseJSON);
+    })
+};
+
+export { AddLoadedUsers, dispatchSignout, getDataAndDispatch };
--- frontend/src/js/actions.ts
+++ frontend/src/js/actions.ts
@@ -5,5 +5,6 @@ const UPDATE_CACHE:string = 'UPDATE_CACHE';
 const ADD_LOADED_USERS:string = 'ADD_LOADED_USERS';
 const HAS_CACHE:boolean = true; // this action is dispacthed when the user stats is properly loaded.
 const HAS_NO_CACHE:boolean = false; // this is fired at the initial stage.
+const USER:string = 'USER';
 
-export {LOADING, NOT_LOADING, UPDATE_CACHE, ADD_LOADED_USERS, HAS_CACHE, HAS_NO_CACHE};
+export {LOADING, NOT_LOADING, UPDATE_CACHE, ADD_LOADED_USERS, HAS_CACHE, HAS_NO_CACHE, USER};
