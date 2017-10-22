15f27d19b4c6b6bc7fda0c1d04c1445d1820ae43
dannymcwaves <dannymcwaves96@gmail.com>
2017-10-22 14:18:12 +0000

    entry point for library components

---
+++ frontend/src/components/lib/index.js
@@ -0,0 +1,5 @@
+
+import {Window, Grid, WindowGrid, GridFluid, WindowGridFluid, Row, WindowRow, Col, AutoCol, ClearFix} from "./containers";
+import {Icon, Stack} from "./icon";
+
+export { Icon, Stack, Window, Row, WindowRow, Grid, GridFluid, WindowGrid, WindowGridFluid, Col, AutoCol, ClearFix}
